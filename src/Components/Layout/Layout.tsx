006b1d0089f444c40d305a826d3093a59360b166
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-01-08 19:02:37 +0530

    backend connected

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '../Navbar/Navbar';
 export default function Layout() {
     return (
         <div className='flex items-center justify-center'>
-            <div className='flex w-[1440px] flex-col'>
+            <div className='flex w-[1440px] flex-col gap-5'>
                 <Navbar />
                 <Routes>
                     <Route path='' element={<Home />} />
