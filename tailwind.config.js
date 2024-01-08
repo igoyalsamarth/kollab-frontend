0e4c4a8b91246f94f78ab9309c3928ccab2a6cbf
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-01-08 17:44:20 +0530

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        moderno: ['MuseoModerno', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
