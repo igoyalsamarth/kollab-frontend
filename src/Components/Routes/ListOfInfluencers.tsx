8f3a90d970d00a9f81c057fe8841dd2612c6028d
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-02-08 13:54:31 +0530

    searchbar update

--- src/Components/Routes/ListOfInfluencers.tsx
+++ src/Components/Routes/ListOfInfluencers.tsx
@@ -9,8 +9,6 @@ import { Link } from 'react-router-dom';
 export default function ListOfInfluencers() {
   const [data, setData] = useState<undefined | any>(undefined)
   const { id } = useParams()
-  const [categoryState, setCategortyState] = useState<string | undefined>(id)
-
 
   useEffect(() => {
     const getUsers = async () => {
@@ -30,7 +28,7 @@ export default function ListOfInfluencers() {
       <div className="flex flex-col items-center gap-[30px]">
         <p className="text-[#2B3031] font-moderno text-[40px]">{data?.length} Creators found</p>
         <div className="relative">
-          <input type="text" value={categoryState} className="w-[721px] capitalize text-[#2B3031] active:outline-[#2B3031] focus:outline-[#2B3031] px-5 py-[10px] border-[3px] border-[#D0D9F1] rounded-full" />
+          <div className="w-[721px] capitalize text-[#2B3031] active:outline-[#2B3031] focus:outline-[#2B3031] px-5 py-[10px] border-[3px] border-[#D0D9F1] rounded-full">{id}</div>
           <Link to='/' className="absolute top-[12px] right-[12px]">
             <svg xmlns="http://www.w3.org/2000/svg" className=" w-6 h-6" viewBox="0 0 24 24" fill="none">
               <path d="M6.3184 6.40767L17.441 17.5589M6.29454 17.5685L17.4315 6.43153" stroke="#4A5354" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
