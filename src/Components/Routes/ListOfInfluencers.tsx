2418788e0e241e2fd977a77c8fa4c4ece9f9fe46
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-02-08 13:51:56 +0530

    minor updates

--- src/Components/Routes/ListOfInfluencers.tsx
+++ src/Components/Routes/ListOfInfluencers.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { serverEndpoint } from "../../util/endpoint";
 import LoadingPageAnimation from "../Loading/LoadingPageAnimation";
 import { useParams } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function ListOfInfluencers() {
   const [data, setData] = useState<undefined | any>(undefined)
@@ -30,11 +31,11 @@ export default function ListOfInfluencers() {
         <p className="text-[#2B3031] font-moderno text-[40px]">{data?.length} Creators found</p>
         <div className="relative">
           <input type="text" value={categoryState} className="w-[721px] capitalize text-[#2B3031] active:outline-[#2B3031] focus:outline-[#2B3031] px-5 py-[10px] border-[3px] border-[#D0D9F1] rounded-full" />
-          <button onClick={() => setCategortyState('')} className="absolute top-[12px] right-[12px]">
+          <Link to='/' className="absolute top-[12px] right-[12px]">
             <svg xmlns="http://www.w3.org/2000/svg" className=" w-6 h-6" viewBox="0 0 24 24" fill="none">
               <path d="M6.3184 6.40767L17.441 17.5589M6.29454 17.5685L17.4315 6.43153" stroke="#4A5354" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
-          </button>
+          </Link>
         </div>
       </div>
       {data ? <div className="grid grid-cols-4 gap-[60px] w-[1140px] py-10">
